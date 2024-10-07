69948f640a132b09e31ab0957614312198278d51
Ayush Namdhar <ayushnamdhar27@gmail.com>
2024-10-07 10:21:34 +0530

    'change'

--- tik/settings.py
+++ tik/settings.py
@@ -28,7 +28,8 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = [os.getenv('ALLOWED_HOSTS')]
+ALLOWED_HOSTS = ['tic-tac-toe-web-socket-6x2z.onrender.com' ,'localhost' ,
+                 '127.0.0.1']
 
 
 # Application definition
